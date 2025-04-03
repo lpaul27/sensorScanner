4e9b0daea27c3b440a49c2b4ece952b99b0fd7d6
lpaul27 <lpaul27@my.whitworth.edu>
2025-04-03 10:50:11 -0700

    push

--- src/main.c
+++ src/main.c
@@ -3,6 +3,8 @@
 #include <zephyr/devicetree.h>
 #include <zephyr/drivers/sensor.h>
 
+struct device *tempSensors[1];
+uint64_t sensorIDs [1] = {0x280305f90c0000cc};
 
 static const struct device *get_ds18b20_device(void){
         const struct device *const dev = DEVICE_DT_GET_ANY(maxim_ds18b20);
@@ -15,12 +17,18 @@ static const struct device *get_ds18b20_device(void){
 
         if(!device_is_ready(dev)){
                 printk("\nError: Device \"%s\" is not ready; "
-                        "check the driver initialization logs forn errors. \n",
+                        "check the driver initialization logs for errors. \n",
                         dev->name);
                 return NULL;
         }
 
         printk("Found device \"%s\", getting sensor data\n", dev->name);
+        printk("test, \"%s\"", dev ->name);
+
+        if (dev->name == "280305f90c0000cc") {
+                printk("Found 1\n");
+                tempSensors[0] = &dev;
+        }
         return dev;
 } // end get function
 
@@ -29,34 +37,41 @@ int main(void){
 
         int res;
 
-        const struct device *dev = get_ds18b20_device();
+        const struct device *dev = get_ds18b20_device();// 280305f90c0000cc// 
 
                 if(dev == NULL){
                         return 0;
                 }
 
+                // loop for gathering the serial number of temperature sensor, NOT FOR DATA COLLECTION OF TEMP
+                while(true){
+                        res = sensor_sample_fetch(dev);
+                        printk("test, res: %d", res);
+                        k_sleep(K_MSEC(20000));
+                }
+                
+                
+
+
 
         while(true){
+                struct sensor_value temp;
+                
                 res = sensor_sample_fetch(dev);
-                k_sleep(K_MSEC(20000));
+                // printk("TEST, dev: %d ", dev->name);
+                if(res != 0){
+                        printk("sample_fetch() failed: %d\n", res);
+                        return res;
+                }
+
+                res = sensor_channel_get(dev, SENSOR_CHAN_AMBIENT_TEMP, &temp);
+                if(res != 0){
+                        printk("channel_get() failed: %d\n", res);
+                        return res;
+                }
+
+                printk("Temp: %d.%06d\n", temp.val1, temp.val2);
+                k_sleep(K_MSEC(2000));
         }
-        // while(true){
-        //         struct sensor_value temp;
-
-        //         res = sensor_sample_fetch(dev);
-        //         if(res != 0){
-        //                 printk("sample_fetch() failed: %d\n", res);
-        //                 return res;
-        //         }
-
-        //         res = sensor_channel_get(dev, SENSOR_CHAN_AMBIENT_TEMP, &temp);
-        //         if(res != 0){
-        //                 printk("channel_get() failed: %d\n", res);
-        //                 return res;
-        //         }
-
-        //         printk("Temp: %d.%06d\n", temp.val1, temp.val2);
-        //         k_sleep(K_MSEC(2000));
-        // }
         return 0;
 }
